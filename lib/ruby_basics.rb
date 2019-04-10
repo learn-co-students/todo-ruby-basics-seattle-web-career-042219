c9ea7239251e93cd72e5ca7b4f62734dd0324f13
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-10 05:58:40 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  variable = value
 end
 
-def argue
-  
+def argue (argue)
+return argue
 end
 
-def greeting
-
+def greeting (greeting, name)
+  return greeting, name
 end
 
-def return_a_value
-
+def return_a_value(value = "Nice")
+return value
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(argument = "cheese")
+  return argument
 end

b6b20941aa8cde3110921b29bf3258d07dbb944c
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-10 23:22:29 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  (num1 / num2)
 end
 
 def assign_variable(value)
-  
+  x = value
 end
 
-def argue
-  
+def argue(words)
+  words
 end
 
-def greeting
-
+def greeting(sent1, sent2)
+  "#{sent1} << #{sent2}"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(words = "cheese")
+  words
 end
